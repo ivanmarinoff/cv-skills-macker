871f84c0df342ffd30f2454bf0de7a640fe8cd45
ivanmarinoff <marinoffivan@gmail.com>
2023-04-30 15:02:37 +0300

    Summary updates

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     packages=find_packages(exclude=[".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["cv_skills_maker = cv_skills_maker.__main__:main"]
+        "console_scripts": ["cv-skills-maker = __main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
